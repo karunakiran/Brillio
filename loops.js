c5d0c0e862beae4f7204d7139d855110059565aa
karunakiran <51288508+karunakiran@users.noreply.github.com>
2021-08-04 22:02:39 +0530

    Add files via upload

---
+++ loops.js
@@ -0,0 +1,25 @@
+x=10
+while(x>5)
+{
+    console.log(x)
+    x--
+}
+
+//do while
+do{
+    console.log("heyy")
+}while(!1)
+
+//for 
+let i
+for(i=5;i>0;i--)
+{
+    console.log(i)
+}
+//another way of for
+arr=[1,2,3,4]
+let t=0
+for(let i in arr)
+{
+     t=t+i
+}console.log(t)
